9d0873a884631884d206bd53b360f1700e67e5d5
elijk88 <94997560+elijk88@users.noreply.github.com>
2021-12-06 13:07:29 -0500

    Update MSSA_Automation_Script.ps1
    I have added code that adds a new AD user account to a connected domain. The user has to select a current OU for the added user.

--- MSSA_Automation_Script.ps1
+++ MSSA_Automation_Script.ps1
@@ -21,7 +21,9 @@ while ($continue) {
     Write-Host "6. Identify accounts without logins >90 days "
     Write-Host "                                  "
     Write-Host "7. Disable accounts without logins >90 days "
-    Write-Host "                                  "  
+    Write-Host "                                  "
+    Write-Host "9. Create a new AD user account   "
+    Write-Host "                                  "
     Write-Host "X. Exit this menu                 "
     Write-Host "                                  "
     $choice = Read-Host  "Enter selection"
@@ -94,6 +96,47 @@ while ($continue) {
         "7" {
 	Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 | ?{$_.enabled -eq $true} |  Disable-ADAccount
         }
+	"9" { 
+		#Request users first name        
+		$userF = Read-Host "Enter the user First name please"
+
+		#Request users last name
+		$userL = Read-Host "Enter the user Last name please"
+	
+		#Organizational Unit the user will be place in
+		$SelectedOU = Read-Host "Please provide an OU" 
+	
+		#Combines users first and last name
+		$CN = $userF + " " + $userL
+	
+		#Takes the first initial of user first name adds a period and user last name (example: F.Lastname)
+		$SamN = $userF.Substring(0,1)+"."+$userL
+	
+		#Retrieves the current domain name
+		$DomainN = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select-Object Domain
+	
+		#Takes the first initial of the user first name and the user last to user principle name (example: F.Lastname@domain.com)
+		$upn = $userF.Substring(0,1)+""+$userL+"@"+$DomainN.Domain
+	
+		#Default location for user
+		$L1 = "OU="+$SelectedOU+",DC=Adatum,DC=com"
+	
+		#Ask user to input a password 
+		$PwReq = Write-Host "Password must meet the following requirement:" -ForegroundColor Yellow
+			 Write-Host "1. password must be at least 8 characters in length" -ForegroundColor Yellow
+			 Write-Host "2. three of the four characters need to be uppercase, lowercase,numbers, or symbols 'n" -ForegroundColor Yellow 
+			 Write-Host "3. Does not contain the user’s username" -ForegroundColor Yellow
+			 Write-Host "                   "
+			 Write-Host "                   "
+			 Read-Host -AsSecureString "Please provide a valid password" 
+					 
+			Try {
+			New-ADUser -Name $CN -GivenName $userF -Surname $userL -SamAccountName $SamN -UserPrincipalName $upn -Path $L1 -AccountPassword $PwReq
+			
+			} catch {
+				  Write-Host "The user $CN already exist in '$DomainN.Domain' domain"
+				  }
+		}              
         "X" {
 	            $continue = $false
 	        }

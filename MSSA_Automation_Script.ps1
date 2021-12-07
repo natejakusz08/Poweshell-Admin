2738ef27f839d0efa6d67a02761991e160d94642
Nicolas <53389645+njlarson40@users.noreply.github.com>
2021-12-07 11:21:59 -0500

    Makes my initial edit domain neutral
    Script asks the user to fill in variables for Target OU path. You must create your GRAVEYARD / STALE USER OU prior to running this script. It assumes your org has a SOP for a phased account deletion.

--- MSSA_Automation_Script.ps1
+++ MSSA_Automation_Script.ps1
@@ -94,7 +94,12 @@ while ($continue) {
 	        Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 | ?{$_.enabled -eq $true} | %{Get-ADUser $_.ObjectGuid} | select name, givenname, surname, Userprincipalname  | out-gridview
                 }
         "7" {
-	        $TargetOU =  "ou=StaleUsers,dc=Adatum,dc=com"
+	    $OU1 = Read-Host "Enter Name of Stale Account OU"
+            $DC1 = Read-Host "Enter Name of Organization Domain"
+            $DC2 = Read-Host "Enter Name of Public Domain"
+        
+            $TargetOU =  "ou=$OU1,dc=$DC1,dc=$DC2"
+            
 		
 		Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 | ?{$_.enabled -eq $true} |  Move-ADObject  -TargetPath $TargetOU
 		Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 | ?{$_.enabled -eq $true} |  Disable-ADAccount
